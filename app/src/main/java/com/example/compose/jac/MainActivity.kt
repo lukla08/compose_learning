7325503de4fea3b91f55f72ea8340aaee27a8c71
Jarosław Chybowski <jaroslaw.chybowski@ekspert.biz>
2024-01-07 16:49:13 +0100

    pierwsze testy

---
+++ app/src/main/java/com/example/compose/jac/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.compose.jac
+
+import android.os.Bundle
+
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MessageCard("user")
+        }
+    }
+}
+
+@Composable
+fun MessageCard(name : String) {
+    Text(text = "Hello $name ")
+}
+
+@Preview
+@Composable
+fun PreviewMessageCard() {
+    MessageCard(" preview 1")
+}
